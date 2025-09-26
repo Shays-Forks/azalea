d48f10765cc5cdd627c5cf2863b42db17e028830
mat <git@matdoes.dev>
2025-09-26 11:28:42 -0400

    update azalea-auth docs and variable names to make it clear that the cache key doesn't need to be an email

--- azalea-auth/src/auth.rs
+++ azalea-auth/src/auth.rs
@@ -3,13 +3,14 @@
 use std::{
     collections::HashMap,
     path::PathBuf,
-    time::{Instant, SystemTime, UNIX_EPOCH},
+    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
 };
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use thiserror::Error;
+use tokio::time::sleep;
 use tracing::{error, trace};
 use uuid::Uuid;
 
@@ -61,22 +62,23 @@ pub enum AuthError {
 /// Authenticate with Microsoft. If the data isn't cached,
 /// they'll be asked to go to log into Microsoft in a web page.
 ///
-/// The email is technically only used as a cache key, so it *could* be
-/// anything. You should just have it be the actual email so it's not confusing
-/// though, and in case the Microsoft API does start providing the real email.
+/// The cache key is an arbitrary string that's used to identify the account in
+/// the future. As a convention, we usually prefer to put the account email
+/// here.
 ///
 /// If you want to use your own code to cache or show the auth code to the user
 /// in a different way, use [`get_ms_link_code`], [`get_ms_auth_token`],
 /// [`get_minecraft_token`] and [`get_profile`] instead.
-pub async fn auth(email: &str, opts: AuthOpts<'_>) -> Result<AuthResult, AuthError> {
+pub async fn auth(cache_key: &str, opts: AuthOpts<'_>) -> Result<AuthResult, AuthError> {
     let cached_account = if let Some(cache_file) = &opts.cache_file {
-        cache::get_account_in_cache(cache_file, email).await
+        cache::get_account_in_cache(cache_file, cache_key).await
     } else {
         None
     };
 
-    if cached_account.is_some() && !cached_account.as_ref().unwrap().mca.is_expired() {
-        let account = cached_account.as_ref().unwrap();
+    if let Some(account) = &cached_account
+        && !account.mca.is_expired()
+    {
         // the minecraft auth data is cached and not expired, so we can just
         // use that instead of doing auth all over again :)
 
@@ -92,7 +94,7 @@ pub async fn auth(email: &str, opts: AuthOpts<'_>) -> Result<AuthResult, AuthErr
         let mut msa = if let Some(account) = cached_account {
             account.msa
         } else {
-            interactive_get_ms_auth_token(&client, email, Some(client_id), Some(scope)).await?
+            interactive_get_ms_auth_token(&client, cache_key, Some(client_id), Some(scope)).await?
         };
         if msa.is_expired() {
             trace!("refreshing Microsoft auth token");
@@ -108,9 +110,13 @@ pub async fn auth(email: &str, opts: AuthOpts<'_>) -> Result<AuthResult, AuthErr
                 Err(e) => {
                     // can't refresh, ask the user to auth again
                     error!("Error refreshing Microsoft auth token: {}", e);
-                    msa =
-                        interactive_get_ms_auth_token(&client, email, Some(client_id), Some(scope))
-                            .await?;
+                    msa = interactive_get_ms_auth_token(
+                        &client,
+                        cache_key,
+                        Some(client_id),
+                        Some(scope),
+                    )
+                    .await?;
                 }
             }
         }
@@ -129,12 +135,12 @@ pub async fn auth(email: &str, opts: AuthOpts<'_>) -> Result<AuthResult, AuthErr
 
         let profile: ProfileResponse = get_profile(&client, &res.minecraft_access_token).await?;
 
-        if let Some(cache_file) = opts.cache_file {
-            if let Err(e) = cache::set_account_in_cache(
+        if let Some(cache_file) = opts.cache_file
+            && let Err(e) = cache::set_account_in_cache(
                 &cache_file,
-                email,
+                cache_key,
                 CachedAccount {
-                    email: email.to_string(),
+                    cache_key: cache_key.to_string(),
                     mca: res.mca,
                     msa,
                     xbl: res.xbl,
@@ -142,9 +148,8 @@ pub async fn auth(email: &str, opts: AuthOpts<'_>) -> Result<AuthResult, AuthErr
                 },
             )
             .await
-            {
-                error!("{}", e);
-            }
+        {
+            error!("{}", e);
         }
 
         Ok(AuthResult {
@@ -328,7 +333,7 @@ pub async fn get_ms_link_code(
 
     Ok(client
         .post("https://login.live.com/oauth20_connect.srf")
-        .form(&vec![
+        .form(&[
             ("scope", scope),
             ("client_id", client_id),
             ("response_type", "device_code"),
@@ -354,17 +359,17 @@ pub async fn get_ms_auth_token(
         CLIENT_ID
     };
 
-    let login_expires_at = Instant::now() + std::time::Duration::from_secs(res.expires_in);
+    let login_expires_at = Instant::now() + Duration::from_secs(res.expires_in);
 
     while Instant::now() < login_expires_at {
-        tokio::time::sleep(std::time::Duration::from_secs(res.interval)).await;
+        sleep(Duration::from_secs(res.interval)).await;
 
         trace!("Polling to check if user has logged in...");
         let res = client
             .post(format!(
                 "https://login.live.com/oauth20_token.srf?client_id={client_id}"
             ))
-            .form(&vec![
+            .form(&[
                 ("client_id", client_id),
                 ("device_code", &res.device_code),
                 ("grant_type", "urn:ietf:params:oauth:grant-type:device_code"),
@@ -375,8 +380,8 @@ pub async fn get_ms_auth_token(
             .await;
         if let Ok(access_token_response) = res {
             trace!("access_token_response: {:?}", access_token_response);
-            let expires_at = SystemTime::now()
-                + std::time::Duration::from_secs(access_token_response.expires_in);
+            let expires_at =
+                SystemTime::now() + Duration::from_secs(access_token_response.expires_in);
             return Ok(ExpiringValue {
                 data: access_token_response,
                 expires_at: expires_at
@@ -390,20 +395,22 @@ pub async fn get_ms_auth_token(
     Err(GetMicrosoftAuthTokenError::Timeout)
 }
 
-/// Asks the user to go to a webpage and log in with Microsoft. If you need to
-/// access the code, then use [`get_ms_link_code`] and then
+/// Asks the user to go to a webpage and log in with Microsoft.
+///
+/// If you need to access the code, then use [`get_ms_link_code`] and then
 /// [`get_ms_auth_token`] instead.
 pub async fn interactive_get_ms_auth_token(
     client: &reqwest::Client,
-    email: &str,
+    cache_key: &str,
     client_id: Option<&str>,
     scope: Option<&str>,
 ) -> Result<ExpiringValue<AccessTokenResponse>, GetMicrosoftAuthTokenError> {
     let res = get_ms_link_code(client, client_id, scope).await?;
     trace!("Device code response: {:?}", res);
+    let verification_uri = &res.verification_uri;
+    let user_code = &res.user_code;
     println!(
-        "Go to \x1b[1m{}\x1b[m and enter the code \x1b[1m{}\x1b[m for \x1b[1m{}\x1b[m",
-        res.verification_uri, res.user_code, email
+        "Go to \x1b[1m{verification_uri}?otc={user_code}\x1b[m and enter the code \x1b[1m{user_code}\x1b[m for \x1b[1m{cache_key}\x1b[m",
     );
 
     get_ms_auth_token(client, res, client_id).await
@@ -428,7 +435,7 @@ pub async fn refresh_ms_auth_token(
 
     let access_token_response_text = client
         .post("https://login.live.com/oauth20_token.srf")
-        .form(&vec![
+        .form(&[
             ("scope", scope),
             ("client_id", client_id),
             ("grant_type", "refresh_token"),
@@ -441,8 +448,7 @@ pub async fn refresh_ms_auth_token(
     let access_token_response: AccessTokenResponse =
         serde_json::from_str(&access_token_response_text)?;
 
-    let expires_at =
-        SystemTime::now() + std::time::Duration::from_secs(access_token_response.expires_in);
+    let expires_at = SystemTime::now() + Duration::from_secs(access_token_response.expires_in);
     Ok(ExpiringValue {
         data: access_token_response,
         expires_at: expires_at
@@ -558,7 +564,7 @@ async fn auth_with_minecraft(
         .await?;
     trace!("{:?}", res);
 
-    let expires_at = SystemTime::now() + std::time::Duration::from_secs(res.expires_in);
+    let expires_at = SystemTime::now() + Duration::from_secs(res.expires_in);
     Ok(ExpiringValue {
         data: res,
         // to seconds since epoch
